619c53dbb4dbb3c1aa59791557a8598ff6c2a6d3
Pedro <pepedro2512@gmail.com>
2023-03-01 18:10:46 -0300

    Changes separating Docker functions in a class

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,11 +12,17 @@ class Docker implements Serializable {
 
   def buildDockerImage(String imageName){
     script.echo "building image..."
+    script.sh "docker build -t $imageName ."
+  }
+
+  def dockerLogin(){
     script.withCredentials([script.userPassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]){
-      script.sh "docker build -t $imageName ."
       script.sh "echo $script.PWD | docker login -u $script.USER --password-stdin"
-      script. sh "docker push $imageName"
     }
+  }
+
+  def dockerPush(String imageName){
+      script. sh "docker push $imageName"
   }
 
 }
